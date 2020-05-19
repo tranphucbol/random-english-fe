21b2183f53f3d3ef9803a87d42cd89b492345831
anhhuy1304 <1653030@student.hcmus.edu.vn>
2020-05-19 21:23:00 +0700

    done route

--- random_english/src/App.js
+++ random_english/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Switch, Route } from 'react-router-dom'
+import './css/tailwind.css'
+import  LoginForm  from "./components/Login-Component";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path='/login' component={LoginForm}/>
+        {/* <Route path='/Register' component={RegisterForm}/> */}
+        <Route path='/' component={LoginForm}/>
+      </Switch>
     </div>
   );
 }
--- random_english/src/components/Login-Component.js
+++ random_english/src/components/Login-Component.js
@@ -1,31 +1,31 @@
 import React from 'react';
 
 const LoginForm = ()=>{
-    return (<div class="w-full max-w-xs">
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+    return (<div className="w-full max-w-xs" style={{margin: '50px auto'}}>
+    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
           Username
         </label>
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
       </div>
-      <div class="mb-6">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+      <div className="mb-6">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
           Password
         </label>
-        <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
-        <p class="text-red-500 text-xs italic">Please choose a password.</p>
+        <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+        <p className="text-red-500 text-xs italic">Please choose a password.</p>
       </div>
-      <div class="flex items-center justify-between">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+      <div className="flex items-center justify-between">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
           Sign In
         </button>
-        <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+        <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
           Forgot Password?
         </a>
       </div>
     </form>
-    <p class="text-center text-gray-500 text-xs">
+    <p className="text-center text-gray-500 text-xs">
       &copy;2020 Acme Corp. All rights reserved.
     </p>
   </div>)
