aedfa9b96667e5d64360277f4e0f0e6b4c3185cb
anhhuy1304 <1653030@student.hcmus.edu.vn>
2020-05-20 21:32:08 +0700

    Updates

---
+++ precache-manifest.b7ff83ce5c889c5484be6f0a0c00de1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe7128bd0dc0c2a78f507a39d40ed15",
+    "url": "/randomEnglish/index.html"
+  },
+  {
+    "revision": "96b71eeef1c82150bdcb",
+    "url": "/randomEnglish/static/css/main.abb64974.chunk.css"
+  },
+  {
+    "revision": "2885ea1d11747e2aaf30",
+    "url": "/randomEnglish/static/js/2.00043145.chunk.js"
+  },
+  {
+    "revision": "773f34b187b0e3e10457763a98a03d2d",
+    "url": "/randomEnglish/static/js/2.00043145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b71eeef1c82150bdcb",
+    "url": "/randomEnglish/static/js/main.cf46234f.chunk.js"
+  },
+  {
+    "revision": "27620b565fda495abb3d",
+    "url": "/randomEnglish/static/js/runtime-main.291292c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/randomEnglish/static/media/logo.5d5d9eef.svg"
+  }
+]);
